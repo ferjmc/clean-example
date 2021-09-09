320a9c9b960512cb64e0b00caffe8adc30069f02
Fernando <ferpierino@gmail.com>
2021-09-09 20:17:34 -0300

    install config and logger

---
+++ cmd/api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/ferjmc/clean-example/config"
+	"github.com/ferjmc/clean-example/pkg/logger"
+)
+
+func main() {
+	log.Println("Starting api server")
+
+	configPath := GetConfigPath(os.Getenv("config"))
+
+	cfgFile, err := config.LoadConfig(configPath)
+	if err != nil {
+		log.Fatalf("LoadConfig: %v", err)
+	}
+
+	cfg, err := config.ParseConfig(cfgFile)
+	if err != nil {
+		log.Fatalf("ParseConfig: %v", err)
+	}
+
+	appLogger := logger.NewApiLogger(cfg)
+
+	appLogger.InitLogger()
+	appLogger.Infof("AppVersion: %s, LogLevel: %s, Mode: %s, SSL: %v", cfg.Server.AppVersion, cfg.Logger.Level, cfg.Server.Mode, cfg.Server.SSL)
+
+}
+
+// Get config path for local or docker
+func GetConfigPath(configPath string) string {
+	if configPath == "docker" {
+		return "./config/config-docker"
+	}
+	return "./config/config-local"
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ferjmc/clean-example
+
+go 1.16
+
+require (
+	github.com/spf13/viper v1.8.1 // indirect
+	go.uber.org/zap v1.19.1 // indirect
+)
